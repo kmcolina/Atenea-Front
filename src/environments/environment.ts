bf30c28f877d7fd1fb0dd20a12b3132cbbb07451
Katherine Colina <52512778+kmcolina@users.noreply.github.com>
2022-08-22 18:03:01 -0400

    Delete src directory

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  baseUri: 'https://glupdesign.com',
-  baseApi: 'https://glupdesign.com/api/auth/',
-  baseApiAuth: 'https://glupdesign.com/api/auth/',
-  keyUsers: 'fwer345g34fk4jh5k34h34jgf4hjfdj345',
-  sessionToken: 'fwefwefwefwewewefrg3454k34k5gger',
-};
